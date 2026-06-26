dd93303a3c6dbd982ac861e501f0948046fd2ad1
jpburigo <64077889+jpburigo@users.noreply.github.com>
2026-06-26 16:01:25 -0300

    Main layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import Header from './Header'
+import { useState } from 'react'
+
+export default function Layout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+      
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+        
+        <main className="flex-1 overflow-auto">
+          <div className="p-4 md:p-8">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
